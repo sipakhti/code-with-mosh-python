a7a8a6f068f18f56d231f1d372427f3e05f0d1d9
Umer Mehmood <476061@gmail.com>
2020-02-01 19:34:25 +0500

    NONE

--- Excersices/Excersice 2.py
+++ Excersices/Excersice 2.py
@@ -4,13 +4,13 @@
 
 
 def fizz_buzz(input):
-    if (input % 5 == 0 and input % 3 == 0):
+    if ((input % 5 == 0) and (input % 3 == 0)):
         return "FizzBuzz"
-    elif (input % 5 == 0):
+    if (input % 5 == 0):
         return "Buzz"
-    elif (input % 3 == 0):
+    if (input % 3 == 0):
         return "Fizz"
-    
+    return input
 
 
 print(fizz_buzz(9), 9)
