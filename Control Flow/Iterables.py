2dab561b3b13acc7450db4e2bf8f1cedb870afcf
Umer Mehmood <476061@gmail.com>
2020-02-01 14:11:49 +0500

    NULL

---
+++ Control Flow/Iterables.py
@@ -0,0 +1,16 @@
+print(type(5))
+print(type(range(5)))
+
+# range class is iterable
+for x in range(5):
+    print(x)
+
+# Lists are Iterable
+for x in [1,2,3,4]:
+    print(x)
+
+# Strings are iterbale
+for x in "Pyhton":
+    print(x)
+
+
