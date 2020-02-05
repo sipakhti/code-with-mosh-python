9abd8ff651dc8ad25babaa8d3a7a4645ea788886
Umer Mehmood <476061@gmail.com>
2020-02-06 01:59:25 +0500

    playing with timeit()

--- Excersices/Excercise 3.py
+++ Excersices/Excercise 3.py
@@ -1,6 +1,8 @@
 from pprint import pprint
-# MY METHOD
-sentence = input("Please Input a string")
+from timeit import timeit
+# MY METHOD (runs faster than MOSH_mod algorithim but slower than MOSH algorithim)
+
+sentence = "This is a sentence of some sorts"  # input("Please Input a string")
 
 count = 0
 for char in sentence:
@@ -11,23 +13,23 @@ for char in sentence:
 
 
 print(winner, count)
-count = 0
-temp = []
-char1 = input("which alphabet do you want to count?")
-for char in sentence.lower():
-    if char == char1.lower():
-        count += 1
+# count = 0
+# temp = []
+# char1 = input("which alphabet do you want to count?")
+# for char in sentence.lower():
+#     if char == char1.lower():
+#         count += 1
 
-print(char1, count)
+# print(char1, count)
 
-# MOSH METHOD(MODIFIED)
+# MOSH METHOD (fastest)
 # char_freq = {}
 # for char in sentence:
 #     if char in char_freq:
 #         char_freq[char] += 1
 #     else:
 #         char_freq[char] = 1
-# MOSH METHOD (MODIFIED)
+# MOSH METHOD (MODIFIED) (slowest)
 # char_freq = {char: sentence.count(char) for char in sentence}
 
 # char_freq_sorted = sorted(
