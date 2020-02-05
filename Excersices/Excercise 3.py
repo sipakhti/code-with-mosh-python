6457f7a1577799f851b1fe3069d7759c778fb056
Umer Mehmood <476061@gmail.com>
2020-02-05 15:34:17 +0500

    tesitng

--- Excersices/Excercise 3.py
+++ Excersices/Excercise 3.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 # MY METHOD
-sentence = "This is a common interview question"
+sentence = input("Please Input a string")
 
 count = 0
 for char in sentence:
@@ -18,11 +18,11 @@ print(winner, count)
 #     else:
 #         char_freq[char] = 1
 # MOSH METHOD (MODIFIED)
-char_freq = {char: sentence.count(char) for char in sentence}
+# char_freq = {char: sentence.count(char) for char in sentence}
 
-char_freq_sorted = sorted(
-    char_freq.items(),
-    key=lambda kv: kv[1],
-    reverse=True)
+# char_freq_sorted = sorted(
+#     char_freq.items(),
+#     key=lambda kv: kv[1],
+#     reverse=True)
 
-print(char_freq_sorted[0])
+# print(char_freq_sorted[0])
