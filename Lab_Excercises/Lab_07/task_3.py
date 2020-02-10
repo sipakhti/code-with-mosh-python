0136347861519215b1f97bd1dc1206b04b33f1ba
Umer Mehmood <476061@gmail.com>
2020-02-10 18:39:55 +0500

    optimizing loops

--- Lab_Excercises/Lab_07/task_3.py
+++ Lab_Excercises/Lab_07/task_3.py
@@ -1,52 +1,84 @@
 import time
-prime = 6700417 # int(input("Enter any number to check prime: "))
-temp = True
-#case1 = """
+from array import array
+
 prime = 6700417
 temp = True
+
+temp_list = list(range(2, prime))
+temp_array = array("I", temp_list)
+
 for_start = time.time_ns()
-for number in range(2, prime):
+
+# NORMAL FOR LOOP
+for number in temp_list:
     if prime % number == 0:
         print(f"{prime} is not a prime number")
         temp = False
         break
 for_end = time.time_ns()
-for_total = for_end - for_start
 
-if temp:
-    print("PRIME")
+for1_total = for_end - for_start
 
-
-#case2 ="""
-prime = 6700417
-temp = True
-count = 2
-count1 = prime//10
-while_start = time.time_ns()
-while count < prime:
-    
-    
-    if prime % count1 == 0:
+# ARRAY FOR LOOP
+for_start = time.time_ns()
+for number in temp_array:
+    if prime % number == 0:
         print(f"{prime} is not a prime number")
         temp = False
         break
-    count1 -= 1
-    
+for_end = time.time_ns()
+
+array_total = for_end - for_start
+
+
+if temp:
+    print("PRIME")
+# OPTIMIZED FOR LOOP FOR PRIME IDENTIFICATION
+opt_prime = int(prime*0.5)+1
+for_start = time.time_ns()
+for number in range(2, opt_prime):
+    if prime % number == 0:
+        print(f"{prime} is not a prime number")
+        temp = False
+        break
+for_end = time.time_ns()
+for2_total = for_end - for_start
+
+
+prime = 6700417
+temp = True
+count = 2
+
+# NORMAL WHILE LOOP
+while_start = time.time_ns()
+while count < prime:
 
     if prime % count == 0:
         print(f"{prime} is not a prime number")
         temp = False
         break
 
-    if count1 < count:
-        break
     count += 1
+
 while_end = time.time_ns()
 while_total = while_end - while_start
 
 if temp:
     print("PRIME")
 
-print(f"FOR: {for_total}   WHILE: {while_total}")
+print(f"FOR: {for1_total}   WHILE: {while_total}")
+print(f"normal FOR loop: {for1_total}", f"optimzed FOR loop: {for2_total}",
+      f"speed factor: {for1_total / for2_total}")
+print(f"normal FOR loop: {for1_total}", f"normal WHILE loop: {while_total}",
+      f"speed factor: {for1_total / while_total}")
+print(f"speed factor: {while_total/for2_total} (WHILE / optimized FOR)")
+print(f"array loop: {array_total} speed factor: {for1_total/array_total} ")
 
-#print(timeit(case1,number=1),timeit(case2,number=1))
+# print(timeit(case1,number=1),timeit(case2,number=1))
+with open("output.txt", "w") as file:
+    file.write(
+        f"normal FOR loop: {for1_total} optimzed FOR loop: {for2_total} speed factor: {for1_total / for2_total}")
+    file.write(
+        f"normal FOR loop: {for1_total} normal WHILE loop: {while_total} speed factor: {for1_total / while_total}")
+    file.write(
+        f"speed factor: {while_total/for2_total} (WHILE / optimized FOR)")
