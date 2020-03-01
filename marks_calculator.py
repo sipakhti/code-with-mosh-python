7b93b24503cc9fb5cef995ad66e4a102e6ec1b39
Umer Mehmood <476061@gmail.com>
2020-03-01 16:29:03 +0500

    Added functionality to save the data to a txt file for database import

--- marks_calculator.py
+++ marks_calculator.py
@@ -1,4 +1,6 @@
 def grader(subject_marks, total_marks=150):
+    """Calculates Grades and returns the grade"""
+
     if 50 <= ((subject_marks * 100) / total_marks) < 60:
         return "E"
     if 60 <= ((subject_marks * 100) / total_marks) < 70:
@@ -12,21 +14,53 @@ def grader(subject_marks, total_marks=150):
     return "F"
 
 
-# marks = {"english": 0, "islamiat": 0, "pakistan_studies": 0, "physics": 0, "biology": 0, "math": 0, "urdu": 0, "total": 0}
-# subjects = ("english", "islamiat","pakistan_studies","physics","biology","math","urdu")
+marks = {"english": 0, "islamiat": 0, "pakistan_studies": 0,
+         "physics": 0, "biology": 0, "math": 0, "urdu": 0, "total": 0}
+subjects = ("english", "islamiat", "pakistan_studies",
+            "physics", "biology", "math", "urdu")
 
-# for subject in subjects:
-#     marks[subject] = int(input(f"Please enter {subject} marks: "))
+# continous execution loop
+while True:
+    # so that the value doesnt stack up after every iteration of the loop
+    marks["total"] = 0
 
-#     while not (0 <= marks[subject] <= 150):
-#         print("Invalid marks!!! \n choose any number between 0 - 150")
-#         marks[subject] = int(input(f"Please enter {subject} marks: "))
-       
+    student_name = input("Enter student name:\n")
 
-# for subject in subjects:
-#     print(f"{subject}: {marks[subject]} grade:{grader(marks[subject])}")
-#     marks["total"] += marks[subject]
+# Input Loop
+    for subject in subjects:
+        # prevents program from crashing in case no value is entered at all
+        try:
+            marks[subject] = int(input(f"Please enter {subject} marks: "))
 
-# print(f"TOTAL: {marks['total']}  grade:{grader(marks['total'],1050)}")
-# print("TOTAL: {}  grade:{}".format(marks["total"],grader(marks["total"],1050)))
+            while not (0 <= marks[subject] <= 150):
+                print("Invalid marks!!! \n choose any number between 0 - 150")
+                marks[subject] = int(input(f"Please enter {subject} marks: "))
+        except ValueError:
+            marks[subject] = 0
 
+# prints out the subjects marks on the console
+    for subject in subjects:
+        print(f"{subject}: {marks[subject]} grade:{grader(marks[subject])}")
+        marks["total"] += marks[subject]
+
+    print(f"TOTAL: {marks['total']}  grade:{grader(marks['total'],1050)}")
+    print("TOTAL: {}  grade:{}".format(
+        marks["total"], grader(marks["total"], 1050)))
+# creates a cummulaive string containig all the student data seperated by comma to be parsed in a database
+    name = f"\r{student_name}"
+    sub = ""
+    for subject in subjects:
+        sub += f",{marks.get(subject,0)}"
+    total = f",{marks['total']},{grader(marks['total'],1050)}\n"
+
+    complete_string = name + sub + total
+
+# Saves the data to the file
+    with open("student_data.txt", "a") as f_obj:
+        f_obj.writelines(complete_string)
+
+# option to break out of the loop
+    exit_cmd = input(
+        "Enter q to quit or anyother key to add marks for another student: ")
+    if exit_cmd.lower() == "q":
+        break
